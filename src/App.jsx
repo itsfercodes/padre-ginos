e5787bacdc75fc501bbc1efd5a96cf90abaffb8f
Fernando Lopez <itsfercodes@gmail.com>
2025-04-09 21:35:50 -0600

    Add static assets and link styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,21 @@ const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza name="Pepperoni" description="Pepperoni, cheese, n stuff" />
-      <Pizza name="Napolitana" description="Mozzarella n Tomato Sauce" />
-      <Pizza name="Hawaiian" description="Ham n Pinapple 🤢" />
+      <Pizza
+        name="Pepperoni"
+        description="Pepperoni, cheese, n stuff"
+        image={"/public/pizzas/pepperoni.webp"}
+      />
+      <Pizza
+        name="Napolitana"
+        description="Mozzarella n Tomato Sauce"
+        image={"/public/pizzas/napolitana.webp"}
+      />
+      <Pizza
+        name="Hawaiian"
+        description="Ham n Pinapple 🤢"
+        image={"/public/pizzas/hawaiian.webp"}
+      />
     </div>
   );
 };
